2d63447bf07fdb3b163edd3ccdb5b04cb7ef7ac0
unknown <wylxhs2017@163.com>
2020-11-28 00:02:02 +0800

    10播放器“

---
+++ 11播放器/scripts.js
@@ -0,0 +1,37 @@
+const player = document.querySelector('.player')
+const video = player.querySelector('.viewer')
+const toggle = player.querySelector('.toggle')
+const skipBtns = player.querySelectorAll('[data-skip]')
+const ranges = player.querySelectorAll('.player__slider')
+const progressBar = player.querySelector('.progress__filled')
+const progress = player.querySelector('.progress')
+function togglePlay(){
+    const method = video.paused ? 'play' : 'pause'
+    video[method]()
+}
+function updateToggle(){
+    const icon = video.paused ? '►' : '❚❚'
+    toggle.textContent = icon
+}
+function skip(){
+    video.currentTime +=parseFloat(this.dataset.skip)
+}
+function handleRange(){
+    video[this.name] = this.value
+}
+function handleProgress(){
+    const percent = (video.currentTime / video.duration) * 100
+    progressBar.style.flexBasis = `${percent}%`
+}
+function handleProgressbar(){
+    
+}
+video.addEventListener('click', togglePlay)
+video.addEventListener('play', updateToggle)
+video.addEventListener('pause', updateToggle)
+video.addEventListener('timeupdate', handleProgress)
+toggle.addEventListener('click', togglePlay)
+skipBtns.forEach(btn => btn.addEventListener('click', skip))
+ranges.forEach(range => range.addEventListener('change', handleRange))
+ranges.forEach(range => range.addEventListener('mousemove', handleRange))
+progress.addEventListener('click', handleProgressbar)
